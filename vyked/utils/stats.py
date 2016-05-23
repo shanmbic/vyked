de2f87168ab3b206ea8bc2d52ed1dbd1abd4c5e2
Shantanu Srivastava <shantanu.srivastava@INTN-TECH-SHANTANU.local>
2016-05-23 12:45:21 +0530

    Memory stats

--- vyked/utils/stats.py
+++ vyked/utils/stats.py
@@ -3,9 +3,11 @@ import asyncio
 import setproctitle
 from collections import defaultdict, deque
 import socket
-
+import resource
 
 class Stats:
+    rusage_denom = 1024.
+
     hostname = socket.gethostbyname(socket.gethostname())
     service_name = '_'.join(setproctitle.getproctitle().split('_')[1:-1])
     http_stats = {'total_requests': 0, 'total_responses': 0, 'timedout': 0, 'total_errors': 0}
@@ -16,6 +18,7 @@ class Stats:
         logd = defaultdict(lambda: 0)
         logd['hostname'] = cls.hostname
         logd['service_name'] = cls.service_name
+        logd['mem_usage'] = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / cls.rusage_denom
 
         for key, value in cls.http_stats.items():
             logd[key] += value
